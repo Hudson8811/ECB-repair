d1aa688bb4116ba29f2bc1dd31d74b5f8df0bcaf
Hudson8811 <hudson88@yandex.ru>
2020-01-11 20:00:50 +0300

    corousel_card-block_table

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,25 @@
 /* my scripts */
+var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        576: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1200: {
+          slidesPerView: 5,
+          spaceBetween: 0,
+        },
+      }
+  })
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,2 @@
 /* my scripts */
-
 //= partials/main.js
