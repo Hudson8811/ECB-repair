f1e00409f9a641f9743af7364857cbd6d9f8b450
Hudson8811 <hudson88@yandex.ru>
2020-01-16 23:41:57 +0300

    changed

--- build/js/main.js
+++ build/js/main.js
@@ -70,3 +70,29 @@ var swiper = new Swiper('.swiper-container-2', {
     },
   }
 });
+
+$(function(){
+
+  // #modal_1 - селектор 1 модального окна
+  // #modal_2 - селектор 2 модального окна, которое необходимо открыть из первого
+  var two_modal = function(id_modal_1,id_modal_2) {
+    // определяет, необходимо ли при закрытии текущего модального окна открыть другое
+    var show_modal_2 = false;
+    // при нажатии на ссылку, содержащей в качестве href селектор модального окна
+    $('a[href="' + id_modal_2 + '"]').click(function(e) {
+      e.preventDefault();
+      show_modal_2 = true;
+      // скрыть текущее модальное окно
+      $(id_modal_1).modal('hide');
+    });
+    // при скрытии текущего модального окна открыть другое, если значение переменной show_modal_2 равно true
+    $(id_modal_1).on('hidden.bs.modal', function (e) {
+      if (show_modal_2) {
+        show_modal_2 = false;
+        $(id_modal_2).modal('show');
+      }
+    })
+  
+  }('#exampleModal','#exampleModal-2');
+  
+  });
